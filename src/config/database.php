ff34244af801327c768ff5f46a62c5b034b0146a
luis obando <laobando.6979@unicesmag.edu.co>
2025-03-26 21:59:28 -0500

    DATABASE PETS

---
+++ src/config/database.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$port = "5432";
+$dbname = "pets";
+$user = "postgres";
+$password = "unicesmag";
+
+$conn = "
+host = $host
+port = $port
+dbname = $dbname
+user = $user
+password = $password"
+
+?>
